a8d8e9216ecc64d1298d0a14d72beefa9ecc16d9
Tsi6 <tsionyazachew4@gmail.com>
2026-05-28 14:26:17 +0300

    Car and Bike classes added

--- src/vehicle.java
+++ src/vehicle.java
@@ -1,15 +1,18 @@
-public class vehicle{
+public abstract class Vehicle {
     private String vehicleId;
     public String brand;
     public String model;
     public double dailyRate;
-    public boolean isavailable;
+    public boolean available;
 
-}
-public Vehicle(String vehicleId, String model, double dailyRate) {
+    public Vehicle(String vehicleId, String brand,
+                   String model, double dailyRate) {
         this.vehicleId = vehicleId;
         this.brand = brand;
         this.model = model;
         this.dailyRate = dailyRate;
         this.available = true;
     }
+
+    public abstract String getType();
+
